e1547a852d3f417eac272be7aa0bc95504994cce
Rumen Kyulyumov <kyulyumov@gmail.com>
2018-04-11 15:26:50 +0300

    Added function for starting the game, added function for star rating system

--- js/app.js
+++ js/app.js
@@ -30,6 +30,25 @@ const memory = (function() {
         'fa-bomb',
         'fa-paper-plane-o'
     ];
+	
+	// Disable main state /deleting body/ and starting the game
+    const hideMain = function(type) {
+        const body = document.querySelector('body');
+        body.classList.remove('main');
+        body.classList.remove(type);
+        startGame();
+    };
+	
+	// Function for stars, depending on clicks
+    const updateStars = function() {
+        if(stars === 1 || moves <= deckArray.length) return;
+        
+        // For every 6 moves over the amount of cards in the deck, there will be minus half star
+        if((moves - deckArray.length) % 6 === 0) {
+            stars -= 0.5;
+            displayStars();
+        }
+    };
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
